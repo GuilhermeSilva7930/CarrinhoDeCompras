133cb735ce0e8e58858f4af2a42b0961eff34dea
Guilherme Ferreira <guilhermeferreira7930@gmail.com>
2022-03-11 18:27:46 -0300

    Navbar 2.0

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,10 +1,24 @@
 import './index.css'
 import Carrinho from '../Carrinho/index'
+import ImgLogin from '../../assets/img/person-circle.svg'
+import Logo from '../../assets/img/hamburger-icon.svg'
+import { Container, IconButton } from '@mui/material'
 
 export default function Navbar() {
-    return(
+    return (
         <header>
-            <Carrinho></Carrinho>
+            <Container sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: 'auto' }}>
+                <div id='logo'>
+                    <img src={Logo} alt="Logo FoodHouse" className='imgLogo' />
+                    <h3 id='logo-text'>FoodHouse</h3>
+                </div>
+                <div>
+                    <IconButton>
+                        <img src={ImgLogin} alt="" className='imgLogin' />
+                    </IconButton>
+                    <Carrinho></Carrinho>
+                </div>
+            </Container>
         </header>
     )
 }
